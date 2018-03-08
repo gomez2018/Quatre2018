76325d65b25aa40b471b11c7e11a26212f7ad10f
MUGIWARAPC\Julio <gomez.desarrollo2018@gmail.com>
2018-03-08 00:39:06 -0400

    ajustes

---
+++ JulioGomez/WebServiceJulioGomez/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceJulioGomez
+{
+    public class Corresponsales
+    {
+        public int COR_CORRESPONSAL_ID { get; set; }
+        public string NOMBRE { get; set; }
+    }
+}
---
+++ JulioGomez/WebServiceJulioGomez/WebService1.asmx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+namespace WebServiceJulioGomez
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public DataSet GetData()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=mugiwaraPC; Initial Catalog=cmmoneycash; Integrated Security=True; User id=sa;Password=123456;";
+            SqlDataAdapter da = new SqlDataAdapter("select * from corresponsales", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            return ds;
+
+        }
+
+        [WebMethod]
+        public DataSet GetCorresponsales()
+        {
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=mugiwaraPC; Initial Catalog=cmmoneycash; Integrated Security=True; User id=sa;Password=123456;";
+            SqlDataAdapter da = new SqlDataAdapter("select C.COR_CORRESPONSAL_ID, C.COR_NOMBRE + ' - ' + CAST(count(O.OFI_CORRESPONSAL_ID) AS varchar) NOMBRE from CORRESPONSALES C inner join OFICINAS O(NOLOCK)ON C.COR_CORRESPONSAL_ID = O.OFI_CORRESPONSAL_ID GROUP BY C.COR_CORRESPONSAL_ID, C.COR_NOMBRE, O.OFI_CORRESPONSAL_ID", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            return ds;
+        }
+
+    }
+}
